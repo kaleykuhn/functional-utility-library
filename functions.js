12495ac7d5901b869fcc537fc14f2037329abc0e
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-29 23:07:55 -0700

    add return part of a slice

--- functions.js
+++ functions.js
@@ -1,1 +1,10 @@
-// Your functions go here!
+// Return part of an Array using Slice
+//A1: array
+//A2: index to start slice
+//A3: index to end slice
+//R: new array
+
+function returnPartArrSlice(arr, beginSlice, endSlice) {
+   slicedArr = arr.slice(beginSlice, endSlice);
+   return slicedArr;
+}
