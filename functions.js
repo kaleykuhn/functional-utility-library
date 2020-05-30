672423e7a7858dc699768d03eb14d1c096f73ce5
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-30 00:06:53 -0700

    add function using splice to remove part of array

--- functions.js
+++ functions.js
@@ -8,3 +8,14 @@ function returnPartArrSlice(arr, beginSlice, endSlice) {
    slicedArr = arr.slice(beginSlice, endSlice);
    return slicedArr;
 }
+var cities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+//Remove part of an array using splice
+//A1: begin splice
+//A2: end splice which is not 0 based
+//R: removed array items
+function removeElemSplice(beginSplice, endSplice) {
+   let array = cities;
+   console.log(array);
+   var splicedArr = array.splice(beginSplice, endSplice);
+   return splicedArr;
+}
