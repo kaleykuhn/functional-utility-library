561e43435974db4e66215dffb9996d5a4323f359
kaleykuhn <61959118+kaleykuhn@users.noreply.github.com>
2020-05-30 00:24:52 -0700

    make changes to removeElemSlice

--- functions.js
+++ functions.js
@@ -1,21 +1,26 @@
 // Return part of an Array using Slice
+var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
 //A1: array
 //A2: index to start slice
-//A3: index to end slice
+//A3: index to end slice which is non-inclusive
 //R: new array
 
-function returnPartArrSlice(arr, beginSlice, endSlice) {
-   slicedArr = arr.slice(beginSlice, endSlice);
+function returnPartArrSlice(beginSlice, endSlice) {
+   let array = inputAnim;
+   console.log(array);
+   var slicedArr = array.slice(beginSlice, endSlice);
    return slicedArr;
 }
+
+// Return part of an array using slice method
 var cities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
-//Remove part of an array using splice
+//
 //A1: begin splice
 //A2: end splice which is not 0 based
 //R: removed array items
-function removeElemSplice(beginSplice, endSplice) {
+function removeElemSlice(beginSlice, endSlice) {
    let array = cities;
    console.log(array);
-   var splicedArr = array.splice(beginSplice, endSplice);
-   return splicedArr;
+   var slicedArr = array.slice(beginSlice, endSlice);
+   return slicedArr;
 }
